deade68dc2f3f54b61c06bb9d8fbe69582d9e521
RyanOrsinger <ryan.orsinger@gmail.com>
2019-08-26 15:03:47 -0500

    fix typo

--- 101-exercises.py
+++ 101-exercises.py
@@ -580,7 +580,7 @@ print("Exercise 40 is correct.")
 
 import math
 # Exercise 41
-# Write a function definition named area_of_circle that takes in a number representing a circle's radius and returns the area of the circl
+# Write a function definition named area_of_circle that takes in a number representing a circle's radius and returns the area of the circle
 
 assert area_of_circle(3) == 28.274333882308138
 assert area_of_circle(5) == 78.53981633974483
