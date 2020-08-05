71d411a5a8c606307ea7285119c80af4c37ca61e
RyanOrsinger <ryan.orsinger@gmail.com>
2020-08-05 18:07:46 -0500

    update tests for exercise 66

--- 101-exercises.py
+++ 101-exercises.py
@@ -990,9 +990,9 @@ print("Exercise 65 is correct.")
 # Exercise 66
 # Write a function definition named get_smallest_number that takes in sequence of numbers and returns the smallest number.
 
-assert get_smallest_number([1, 2, 3]) == 1
-assert get_smallest_number([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]) == -5
-assert get_smallest_number([-4, -3, 1]) == -4
+assert get_smallest_number([1, 3, 2]) == 1
+assert get_smallest_number([5, -5, -4, -3, -2, -1, 1, 2, 3, 4]) == -5
+assert get_smallest_number([-4, -3, 1, -10]) == -4
 print("Exercise 66 is correct.")
 
 
